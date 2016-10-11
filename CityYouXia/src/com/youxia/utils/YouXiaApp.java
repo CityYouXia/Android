d66c42359db961bc730b9d425c54a77f3602e21d
WindHors <18729067740@163.com>
2016-10-11 22:29:10 +0800

    press

--- CityYouXia/src/com/youxia/utils/YouXiaApp.java
+++ CityYouXia/src/com/youxia/utils/YouXiaApp.java
@@ -26,7 +26,7 @@ public class YouXiaApp extends Application
 		//短信SMS
 //		SMSSDK.initSDK(this, "1754c2e667a16", "ec8508af82d1b3e8b5e1137d72041439");
 		//腾讯Bugly初始化
-		CrashReport.initCrashReport(getApplicationContext(), "900053741", false);
+		CrashReport.initCrashReport(getApplicationContext(), "474b2320a8", false);
 		//百度地图使用 
 		SDKInitializer.initialize(this);
 		//极光推送
