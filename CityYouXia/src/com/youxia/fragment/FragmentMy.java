e7f0bc1a6f83639e41cffd7a111ce1a8107bbb9b
WindHors <18729067740@163.com>
2016-10-19 20:53:10 +0800

    homepage

--- CityYouXia/src/com/youxia/fragment/FragmentMy.java
+++ CityYouXia/src/com/youxia/fragment/FragmentMy.java
@@ -1,13 +1,18 @@
 package com.youxia.fragment;
 
+import net.tsz.afinal.FinalActivity;
+import net.tsz.afinal.annotation.view.ViewInject;
+
 import com.youxia.BaseFragment;
 import com.youxia.R;
+import com.youxia.widget.NumImageView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 @SuppressLint("InflateParams")
 public class FragmentMy extends BaseFragment {
@@ -16,11 +21,17 @@ public class FragmentMy extends BaseFragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_my, container, false);
+		View root = inflater.inflate(R.layout.fragment_my, container, false);
+		FinalActivity.initInjectedView(this,root);
+		return root;
 	}
 	
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
+		initView();
+	}
+
+	private void initView() {
 	}
 }
--- CityYouXia/src/com/youxia/utils/YouXiaApp.java
+++ CityYouXia/src/com/youxia/utils/YouXiaApp.java
@@ -22,8 +22,6 @@ public class YouXiaApp extends Application
 		mFinalBitmap = FinalBitmap.create(this);
 		//初始化调用一次即可
 		PreferencesUtils.init("SolarKE_YunWei.Golbal", MODE_MULTI_PROCESS);
-		//短信SMS
-//		SMSSDK.initSDK(this, "1754c2e667a16", "ec8508af82d1b3e8b5e1137d72041439");
 		//腾讯Bugly初始化
 		CrashReport.initCrashReport(getApplicationContext(), "474b2320a8", false);
 		//百度地图使用 
